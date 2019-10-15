e491f3b7570db944dfd074514b478f54e0e6834a
farheen.k <farheen.k@FWS-LP-138.flatworld.com>
2019-10-15 12:43:05 +0530

    Commit success

---
+++ Test Suites/Test suite1.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test suite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>456ba701-59bd-442b-9c29-9f9e059ab041</testSuiteGuid>
+   <testCaseLink>
+      <guid>7ed0b937-3de5-4d5f-89cf-9f3d21a41c24</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test case 1</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
